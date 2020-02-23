bbef402a48f47b271bc89d4119ef69b55a73c829
sridharke7 <sridhar.kesavan@outlook.com>
2020-02-23 18:03:59 +0900

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="#/">Post Your Thoughts </a>
+    </nav>
+    <router-view class="container"/>
+  </div>
+</template>
+
+<style>
+</style>
